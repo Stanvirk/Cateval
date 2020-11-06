02000f7a7be15575d9bcae8d5b4cf1deeaaa2ff0
Stan Virk <Stan.Virk@netgroup.com>
2020-11-06 20:25:55 +0200

    Config added.

--- CatevalAPI/app.js
+++ CatevalAPI/app.js
@@ -1,8 +1,15 @@
+//setting enviromental parameters in code instead of system enviroments
+// this part is used only for display purposes to avoid pollution system varaibles
+// -----------------------------------------------------
+process.env['NODE_ENV'] = 'development';
+// -----------------------------------------------------
+
 const express = require('express');
+const config = require('config');
 const admin = require('./routes/admin');
 
 //TODO: move configuration to separate file/module
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.get('port') || 3000;
 
 const app = express();
 app.use(express.json());
